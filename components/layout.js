695874b178dce913f02a4e4d1399b9a1004ad3d4
Arthur <chillout885@gmail.com>
2022-04-23 13:36:09 -0700

    updated css

--- components/layout.js
+++ components/layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Next.js blog demo"
